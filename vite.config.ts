4a7433486a02bd18e608a8cfa96f3374f32719b0
“masazumi0526git config --global user.email “n_zumi529@icloud.com <n_zumi529@icloud.com>
2025-02-24 10:43:48 +0900

    vite.config.ts を修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Meet_Masazumi/',  // GitHub Pages 用にベースパスを設定
 })
