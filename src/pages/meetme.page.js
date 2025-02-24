4e7dc55c24cec825a94a91f5b4be5c68f328368d
“masazumi0526git config --global user.email “n_zumi529@icloud.com <n_zumi529@icloud.com>
2025-02-24 10:52:07 +0900

    ビルド & デプロイ

---
+++ src/pages/meetme.page.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import styles from './meetmepage.module.css';
+export default function Page() {
+    return (_jsxs("div", { className: styles['page-container'], children: [_jsx("h1", { children: "\u4E2D\u7530\u96C5\u6F84\uFF08Masazumi Nakata\uFF09" }), _jsx("p", { children: "\u3053\u3093\u306B\u3061\u306F\uFF01\u79C1\u306FWeb\u958B\u767A\u8005\u3092\u76EE\u6307\u3057\u3066\u3044\u307E\u3059\u3002" }), _jsx("ul", { children: _jsx("li", { children: "\u8DA3\u5473: \u30B5\u30C3\u30AB\u30FC" }) })] }));
+}
