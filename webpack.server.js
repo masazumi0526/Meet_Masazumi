756c1d28051c0870141a49b92b035cfc0ff045dd
“masazumi0526git config --global user.email “n_zumi529@icloud.com <n_zumi529@icloud.com>
2025-02-26 21:33:08 +0900

    node dist/server.cjs パターン

--- webpack.server.js
+++ webpack.server.js
@@ -4,10 +4,14 @@ import nodeExternals from 'webpack-node-externals';
 export default {
   entry: './server/index.js',
   target: 'node',
-  externals: [nodeExternals()],
+  externals: [nodeExternals()], // node_modules をバンドルしない
   output: {
     path: path.resolve('dist'),
-    filename: 'server.js'
+    filename: 'server.js',
+    libraryTarget: 'module', // 出力を ES Module にする
+  },
+  experiments: {
+    outputModule: true, // ES Module の出力を許可
   },
   module: {
     rules: [
@@ -15,12 +19,12 @@ export default {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js']
-  }
+    extensions: ['.js'],
+  },
 };
